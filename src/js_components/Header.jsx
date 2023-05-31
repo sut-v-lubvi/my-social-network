2e888033967eab9f89a79129c466fc5be5862ec2
cipher <sut-v-lubvi@yandex.ru>
2023-05-31 11:13:38 +0300

    create_proj

---
+++ src/js_components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+     <header>
+			<div>
+			<img src="https://svgsilh.com/svg_v2/3419889.svg" alt=""></img>
+			</div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/js_components/NavMeny.jsx
@@ -0,0 +1,23 @@
+const NavMeny = () => {
+	return (
+		<div className="Navigation__linc" >
+			<div className="link">
+			<a href="#">Profile</a>
+			</div>
+			<div className="link">
+			<a href="#">Messages</a>
+			</div>
+			<div className="link">
+			<a href="#">Nevs</a>
+			</div>
+			<div className="link">
+			<a href="#">Music</a>
+			</div>
+			<div className="link">
+			<a href="#">Settings</a>
+			</div>
+		</div>
+		
+	);
+}
+export default NavMeny;
